03d7604aacd72fb1c86936b6c5083d2d28bc317e
Andy Rothstein <arothste@caltech.edu>
2018-07-27 13:30:55 -0700

    Array boundary fixes

--- azimuthal_profile.py
+++ azimuthal_profile.py
@@ -36,9 +36,9 @@ def make_profiles2(a_arr, r_arr, cdens_arr, field, a_bins, r_bins):
   r_bin_ids = np.digitize(r_arr, r_bins)
   a_bin_ids = np.digitize(a_arr, a_bins)
   radii = [0, 11, 21, 31]
-  profile_data = [np.zeros([3, int(len(a_bins) / 3)]), np.zeros([3, \
-                    int(len(a_bins) / 3)]),np.zeros([3, int(len(a_bins) / 3)])]
-  for i, a_bin_id in enumerate(np.arange(int(len(a_bins)/3))):
+  profile_data = [np.zeros([3, len(a_bins)]), np.zeros([3, len(a_bins)]), \
+                  np.zeros([3, len(a_bins)])]
+  for i, a_bin_id in enumerate(np.arange(len(a_bins))):
     for j in range(len(radii)-1):
       ids = np.logical_and(a_bin_ids == a_bin_id, r_bin_ids > radii[j])
       ids = np.logical_and(ids, r_bin_ids <= radii[j+1])
