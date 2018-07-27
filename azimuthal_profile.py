a136e3ffe4c81c0245cbb3a7586f8678c2869a3a
Andy Rothstein <arothste@caltech.edu>
2018-07-27 14:35:13 -0700

    Debuggo

--- azimuthal_profile.py
+++ azimuthal_profile.py
@@ -91,9 +91,10 @@ if __name__ == '__main__':
         f = h5.File(v[j], 'r')
         a_arr = np.concatenate((a_arr, f['phi'].value))
         r_arr = np.concatenate((r_arr, f['radius'].value))
-        if (j%1000) == 0:
-          print(f["%s/%s" % (field, 'edge')].value)
         cdens_arr = np.concatenate((cdens_arr, f["%s/%s" % (field, 'edge')].value))
+        for i in range(len(a_arr)):
+          if cdens_arr[i] > 1e-4:
+            print(cdens_arr[i])
       profile_data = make_profiles2(a_arr, r_arr, cdens_arr, field, a_bins, r_bins)
       
       # profile_data = make_profiles(a_arr, cdens_arr, a_bins, field, n_bins)
