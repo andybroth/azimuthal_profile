3bf3a5a559bb3631de3842a98076cd3297adc1c1
Andy Rothstein <arothste@caltech.edu>
2018-07-31 10:39:27 -0700

    Flip radial bins

--- azimuthal_profile.py
+++ azimuthal_profile.py
@@ -121,7 +121,8 @@ if __name__ == '__main__':
 
   a_n_bins = 30
   r_n_bins = 31
-  r_bins = np.linspace(0, 150, r_n_bins)
+  r_bins = np.linspace(150, 0, r_n_bins)
+  r_bins = np.flip(r_bins, 0)
   a_bins = np.linspace(90, 0, a_n_bins, endpoint=False)
   a_bins = np.flip(a_bins, 0)
 
