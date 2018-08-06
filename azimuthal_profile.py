4dae71a0b62124c59e4f023e8a48390e1c6ba3b7
Andy Rothstein <arothste@caltech.edu>
2018-08-06 15:34:29 -0700

    Added new plot

--- azimuthal_profile.py
+++ azimuthal_profile.py
@@ -82,7 +82,7 @@ def make_radial_profile(a_arr, r_arr, cdens_arr, a_bins, r_bins, a_n_bins, r_n_b
       profile_data[j][2,i] = np.percentile(sample, 75)
   return profile_data
 
-def radial_profile_unadjusted(a_arr, r_arr, cdens_arr, a_bins, r_bins, a_n_bins, r_n_bins):
+def angle_profile_big(a_arr, r_arr, cdens_arr, a_bins, r_bins, a_n_bins, r_n_bins):
   '''
   Makes 3 bins for angle and radius and plots median point for each with
   error bars of 1 standard deviation.
@@ -121,16 +121,16 @@ def fplot_radius(ion):
   plt.savefig('plots/%s_radial.png' % ion)
   plt.clf()
 
-def fplot_new(ion):
+def fplot_big_angle(ion):
   plt.title('%s' % ion)
-  plt.xlabel('Azimuthal Angle [Degrees}')
+  plt.xlabel('Azimuthal Angle [Degrees]')
   plt.xlim((0,90))
   plt.legend(title='Impact Parameter')
-  print('%s_new.png' % ion)
-  plt.savefig('plots/%s_new.png' % ion)
+  print('%s_big_angle.png' % ion)
+  plt.savefig('plots/%s_big_angle.png' % ion)
   plt.clf()
 
-def plot_radial(angle_data, cden_data, label, color, marker):
+def plot_big_angle(angle_data, cden_data, label, color, marker):
   '''
   Plots the azimuthal angle vs Cden for 3 angular and 3 radial bins along with
   a error bar of 1 std for each axis at each point
@@ -142,6 +142,49 @@ def plot_radial(angle_data, cden_data, label, color, marker):
   # plots points
   plt.errorbar(angle_data[0,:], cden_data[0,:], xerr=angle_data[1,:], yerr=yerr_lower, marker=marker, label=label)
   plt.semilogy()
+
+def radial_profile_big(a_arr, r_arr, cdens_arr, a_bins, r_bins, a_n_bins, r_n_bins):
+  '''
+  Makes 2 bins for angle and 4 for radius and plots median point for each with
+  error bars of 1 standard deviation.
+  '''
+  r_bin_ids = np.digitize(r_arr, r_bins)
+  a_bin_ids = np.digitize(a_arr, a_bins)
+  cden_data = [np.zeros([2, a_n_bins]), np.zeros([2, a_n_bins]), \
+                  np.zeros([2, a_n_bins])]
+  radius_data = [np.zeros([2, a_n_bins]), np.zeros([2, a_n_bins]), \
+                np.zeros([2, a_n_bins])]
+  for r_bin_id in range(r_n_bins):
+    for a_bin_id in range(a_n_bins):
+      ids = np.logical_and(r_bin_ids == r_bin_id, a_bin_ids == a_bin_id)
+      sample = cdens_arr[ids]
+      cden_data[r_bin_id][0,a_bin_id] = np.median(sample)
+      cden_data[r_bin_id][1,a_bin_id] = np.std(sample)
+      radius_data[r_bin_id][0,a_bin_id] = np.median(r_arr[ids])
+      radius_data[r_bin_id][1,a_bin_id] = np.std(r_arr[ids])
+  return cden_data, radius_data
+
+def plot_big_radius(radius_data, cden_data, label, color, marker):
+  '''
+  Plots b vs Cden for 2 angular and 4 radial bins along with
+  a error bar of 1 std for each axis at each point
+  '''
+  # makes sure the error bars never go below 0
+  ylower = np.maximum(1e-29, cden_data[0,:] - cden_data[1,:])
+  yerr_lower = cden_data[0,:] - ylower
+  
+  # plots points
+  plt.errorbar(radius_data[0,:], cden_data[0,:], xerr=radius_data[1,:], yerr=yerr_lower, marker=marker, label=label)
+  plt.semilogy()
+
+def fplot_big_radius(ion):
+  plt.title('%s' % ion)
+  plt.xlabel('b [kpc]')
+  plt.xlim((0,90))
+  plt.legend(title='Azimuthal Angle')
+  print('%s_big_radius.png' % ion)
+  plt.savefig('plots/%s_big_radius.png' % ion)
+  plt.clf()
 
 if __name__ == '__main__':
   """
@@ -217,15 +260,31 @@ if __name__ == '__main__':
                     (30*i, 30*i + 30), colors[3*i])
       fplot_radius(ion)
 
-      # Make plot similar to paper
+      # Make plot similar to paper of phi vs N
       r_n_bins = 3
       r_bins = np.linspace(80, 20, r_n_bins, endpoint=False)
       r_bins = np.flip(r_bins, 0)
 
-      cden_data, angle_data = radial_profile_unadjusted(a_arr, r_arr, cdens_arr, a_bins, r_bins, a_n_bins, r_n_bins)
+      cden_data, angle_data = angle_profile_big(a_arr, r_arr, cdens_arr, a_bins, r_bins, a_n_bins, r_n_bins)
       ion = finish_plot(field, COS_data, fn_head)
-      plot_radial(angle_data[0], cden_data[0], 'b < 40 kpc', colors[0], markers[0])
+      plot_big_angle(angle_data[0], cden_data[0], 'b < 40 kpc', colors[0], markers[0])
       for i in range(1,3):
-        plot_radial(angle_data[i], cden_data[i], '%s < b < %s kpc' % \
+        plot_big_angle(angle_data[i], cden_data[i], '%s < b < %s kpc' % \
                     (i*20 + 20, i*20 + 40), colors[3*i], markers[i])
-      fplot_new(ion)
+      fplot_big_angle(ion)
+
+      # Makes plot similar to paper of b vs N
+      r_n_bins = 4
+      r_bins = np.linspace(80, 20, r_n_bins, endpoint=False)
+      r_bins = np.flip(r_bins, 0)
+      a_n_bins = 2
+      a_bins = np.linspace(90, 0, a_n_bins, endpoint=False)
+      a_bins = np.flip(a_bins, 0)
+
+      cden_data, radius_data = radial_profile_big(a_arr, r_arr, cdens_arr, a_bins, r_bins, a_n_bins, r_n_bins)
+      ion = finish_plot(field, COS_data, fn_head)
+      plot_big_radius(radius_data[0], cden_data[0], 'Φ < 45 degrees', colors[0], markers[0])
+      plot_big_radius(radius_data[1], cden_data[1], 'Φ > 45 degrees', colors[3], markers[1])
+      fplot_big_radius(ion)
+
+
