e356b6d7f12b32954a08156cd782e0bd733a95a4
Andy Rothstein <arothste@caltech.edu>
2018-08-07 15:32:05 -0700

    amiga data

--- azimuthal_projections.py
+++ azimuthal_projections.py
@@ -145,8 +145,8 @@ if __name__ == '__main__':
 
 		# Figure out centroid and r_vir info
 		log("Reading amiga center for halo in %s" % fn)
+		log(amiga_data)
 		c = read_amiga_center(amiga_data, fn, ds)
-		log(c)
 		rvir = read_amiga_rvir(amiga_data, fn, ds)
 
 		cdens_file = h5.File(cdens_fn, 'a')
