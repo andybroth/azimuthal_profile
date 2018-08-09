afe8b27e0d119c3ec05f2d8cd138c370d2cb01db
Andy Rothstein <arothste@caltech.edu>
2018-08-09 11:50:43 -0700

    Changed hard coded array

--- azimuthal_profile.py
+++ azimuthal_profile.py
@@ -71,7 +71,7 @@ def make_radial_profile(a_arr, r_arr, cdens_arr, a_bins, r_bins, a_n_bins, r_n_b
   r_bin_ids = np.digitize(r_arr, r_bins)
   a_bin_ids = np.digitize(a_arr, a_bins)
   angle_bins = range(a_n_bins)
-  profile_data = [np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins])]
+  profile_data = [np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins])]
   '''
   for j in range(a_n_bins-1):
     profile_data = np.stack((profile_data, np.zeros([3, r_n_bins])))
