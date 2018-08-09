526c2e086a62e7bec90dd08d8d24514f7722b3b1
Andy Rothstein <arothste@caltech.edu>
2018-08-09 16:57:32 -0700

    Cleaned up code

--- azimuthal_profile.py
+++ azimuthal_profile.py
@@ -28,22 +28,31 @@ sys.path.insert(0, '/home/andyr/src/frb')
 from get_COS_data import get_COS_data, plot_COS_data
 from radial_profile2 import *
 
-def make_profiles2(a_arr, r_arr, cdens_arr, a_bins, r_bins):
-  """
-  Splits data into three radial groups (50 kpc, 100 kpc, 150 kpc) and bins data
-  with respect to azimuthal angle to find the percentiles of each raidal group.
-  """
+def make_profiles2(a_arr, a_bins, a_n_bins, cdens_arr, r_arr, r_bins, r_n_bins, normalize)
+  '''
+  Default is angle vs N plot w/ 3 radial bins in legend. normalize should be set
+  to true for this to work. To make radius vs N plots, flip all angular and radius
+  inputs around cdens_arr and set normalize to false.
+  '''
   r_bin_ids = np.digitize(r_arr, r_bins)
   a_bin_ids = np.digitize(a_arr, a_bins)
-  radii = [0, 10, 20]
-  profile_data = [np.zeros([3, len(a_bins)]), np.zeros([3, len(a_bins)]), \
-                  np.zeros([3, len(a_bins)])]
-  for i, a_bin_id in enumerate(np.arange(len(a_bins))):
-    for j in range(len(radii)):
-      sample = normalize_by_radius(cdens_arr, r_bin_ids, radii[j], a_bin_ids, a_bin_id)
-      profile_data[j][0,i] = np.median(sample)
-      profile_data[j][1,i] = np.percentile(sample, 25)
-      profile_data[j][2,i] = np.percentile(sample, 75)
+  if normalize:
+    radii = [0, 10, 20]
+  else:
+    radii = range(a_n_bins)
+
+  profile_data = [np.zeros(3, a_n_bins) for _ in range(r_n_bins)]
+
+  for a_bin_id in range(a_n_bins):
+    for j, radius in enumerate(radii):
+      if normalize:
+        sample = normalize_by_radius(cdens_arr, r_bin_ids, radius, a_bin_ids, a_bin_id)
+      else:
+        ids = np.logical_and(r_bin_ids == r_bin_id, a_bin_ids == a_bin_id)
+        sample = cdens_arr[ids]
+      profile_data[j][0,a_bin_id] = np.median(sample)
+      profile_data[j][1,a_bin_id] = np.percentile(sample, 25)
+      profile_data[j][2,a_bin_id] = np.percentile(sample, 75)
   return profile_data
 
 def normalize_by_radius(cdens_arr, r_bin_ids, r, a_bin_ids, a_bin_id):
@@ -63,75 +72,49 @@ def normalize_by_radius(cdens_arr, r_bin_ids, r, a_bin_ids, a_bin_id):
     radial_bin_id += 1
   return sample
 
-def make_radial_profile(a_arr, r_arr, cdens_arr, a_bins, r_bins, a_n_bins, r_n_bins):
+def big_profile(a_arr, a_bins, a_n_bins, cdens_arr, r_arr, r_bins, r_n_bins):
   '''
-  Makes a profile based on radius with 3 bins of azimuthal angles: 0-30, 30-60, 
-  60-90. 
+  Makes profile of data with error bars. When parameters are copied as above, it
+  makes different angular bins which show up in legend. To make radial bins, just
+  flip all the angular and radial inputs around the cdens_arr input.
   '''
   r_bin_ids = np.digitize(r_arr, r_bins)
   a_bin_ids = np.digitize(a_arr, a_bins)
-  angle_bins = range(a_n_bins)
-  profile_data = [np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins]),np.zeros([3, r_n_bins])]
-  '''
-  for j in range(a_n_bins-1):
-    profile_data = np.stack((profile_data, np.zeros([3, r_n_bins])))
-  '''
-  for r_bin_id in range(r_n_bins):
-    for j in range(len(profile_data)):
-      ids = np.logical_and(r_bin_ids == r_bin_id, a_bin_ids == angle_bins[j])
-      sample = cdens_arr[ids]
-      import pdb; pdb.set_trace
-      profile_data[j][0,r_bin_id] = np.median(sample)
-      profile_data[j][1,r_bin_id] = np.percentile(sample, 25)
-      profile_data[j][2,r_bin_id] = np.percentile(sample, 75)
-  return profile_data
+  profile_data = [np.zeros(3, r_n_bins) for _ in range(a_n_bins)]
 
-def angle_profile_big(a_arr, r_arr, cdens_arr, a_bins, r_bins, a_n_bins, r_n_bins):
-  '''
-  Makes 3 bins for angle and radius and plots median point for each with
-  error bars of 1 standard deviation.
-  '''
-  r_bin_ids = np.digitize(r_arr, r_bins)
-  a_bin_ids = np.digitize(a_arr, a_bins)
-  cden_data = [np.zeros([2, a_n_bins]), np.zeros([2, a_n_bins]), \
-                  np.zeros([2, a_n_bins])]
-  angle_data = [np.zeros([2, a_n_bins]), np.zeros([2, a_n_bins]), \
-                np.zeros([2, a_n_bins])]
   for r_bin_id in range(r_n_bins):
     for a_bin_id in range(a_n_bins):
       ids = np.logical_and(r_bin_ids == r_bin_id, a_bin_ids == a_bin_id)
       sample = cdens_arr[ids]
-      cden_data[r_bin_id][0,a_bin_id] = np.median(sample)
-      cden_data[r_bin_id][1,a_bin_id] = np.std(sample)
-      angle_data[r_bin_id][0,a_bin_id] = np.median(a_arr[ids])
-      angle_data[r_bin_id][1,a_bin_id] = np.std(a_arr[ids])
-  return cden_data, angle_data
+      profile_data[a_bin_id][0,r_bin_id] = np.median(sample)
+      profile_data[a_bin_id][1,r_bin_id] = np.percentile(sample, 25)
+      profile_data[a_bin_id][2,r_bin_id] = np.percentile(sample, 75)
+  return profile_data
 
-def fplot_angle(ion):
+def fplot_angle(ion, description):
   plt.title('%s' % ion)
   plt.xlabel('Azimuthal Angle [degrees]')
   plt.xlim((0,90))
   plt.legend(title='Radius')
-  print('%s_angle.png' % ion)
-  plt.savefig('plots/%s_angle.png' % ion)
+  if len(description) > 0:
+      print('%s_big_angle.png' % ion)
+    plt.savefig('plots/%s_%s_angle.png' % (ion, description))
+  else:
+    print('%s_angle.png' % ion)
+    plt.savefig('plots/%s_angle.png' % ion)
   plt.clf()
 
-def fplot_radius(ion):
+def fplot_radius(ion, description):
   plt.title('%s' % ion)
   plt.xlabel('Impact Parameter [kpc]')
   plt.xlim((0,50))
   plt.legend(title='Azimuthal Angle')
-  print('%s_radial.png' % ion)
-  plt.savefig('plots/%s_radial.png' % ion)
-  plt.clf()
-
-def fplot_big_angle(ion):
-  plt.title('%s' % ion)
-  plt.xlabel('Azimuthal Angle [Degrees]')
-  plt.xlim((0,90))
-  plt.legend(title='Impact Parameter')
-  print('%s_big_angle.png' % ion)
-  plt.savefig('plots/%s_big_angle.png' % ion)
+  if len(description) > 0:
+    print('%s_%s_radius.png' % (ion, description))
+    plt.savefig('plots/%s_big_radius.png' % ion)
+  else:
+    print('%s_radial.png' % ion)
+    plt.savefig('plots/%s_radial.png' % ion)
   plt.clf()
 
 def plot_big_angle(angle_data, cden_data, label, color, marker):
@@ -147,25 +130,6 @@ def plot_big_angle(angle_data, cden_data, label, color, marker):
   plt.errorbar(angle_data[0,:], cden_data[0,:], xerr=angle_data[1,:], yerr=yerr_lower, marker=marker, label=label)
   plt.semilogy()
 
-def radial_profile_big(a_arr, r_arr, cdens_arr, a_bins, r_bins, a_n_bins, r_n_bins):
-  '''
-  Makes 2 bins for angle and 4 for radius and plots median point for each with
-  error bars of 1 standard deviation.
-  '''
-  r_bin_ids = np.digitize(r_arr, r_bins)
-  a_bin_ids = np.digitize(a_arr, a_bins)
-  cden_data = [np.zeros([2, r_n_bins]), np.zeros([2, r_n_bins])]
-  radius_data = [np.zeros([2, r_n_bins]), np.zeros([2, r_n_bins])]
-  for r_bin_id in range(r_n_bins):
-    for a_bin_id in range(a_n_bins):
-      ids = np.logical_and(r_bin_ids == r_bin_id, a_bin_ids == a_bin_id)
-      sample = cdens_arr[ids]
-      cden_data[a_bin_id][0,r_bin_id] = np.median(sample)
-      cden_data[a_bin_id][1,r_bin_id] = np.std(sample)
-      radius_data[a_bin_id][0,r_bin_id] = np.median(r_arr[ids])
-      radius_data[a_bin_id][1,r_bin_id] = np.std(r_arr[ids])
-  return cden_data, radius_data
-
 def plot_big_radius(radius_data, cden_data, label, color, marker):
   '''
   Plots b vs Cden for 2 angular and 4 radial bins along with
@@ -178,15 +142,6 @@ def plot_big_radius(radius_data, cden_data, label, color, marker):
   # plots points
   plt.errorbar(radius_data[0,:], cden_data[0,:], xerr=radius_data[1,:], yerr=yerr_lower, marker=marker, label=label)
   plt.semilogy()
-
-def fplot_big_radius(ion):
-  plt.title('%s' % ion)
-  plt.xlabel('b [kpc]')
-  plt.xlim((0,90))
-  plt.legend(title='Azimuthal Angle')
-  print('%s_big_radius.png' % ion)
-  plt.savefig('plots/%s_big_radius.png' % ion)
-  plt.clf()
 
 if __name__ == '__main__':
   """
@@ -235,12 +190,12 @@ if __name__ == '__main__':
       r_bins = np.linspace(150, 0, r_n_bins, endpoint=False)
       r_bins = np.flip(r_bins, 0)
 
-      profile_data = make_profiles2(a_arr, r_arr, cdens_arr, a_bins, r_bins)
+      profile_data = make_profiles2(a_arr, a_bins, a_n_bins, cdens_arr, r_arr, r_bins, r_n_bins, True)
       for i in range(3):
         plot_profile(np.linspace(0, 90, a_n_bins), profile_data[i], '%s < b < %s kpc' % \
                     (50*i, 50*i + 50), colors[3*i])
       ion = finish_plot(field, COS_data, fn_head)
-      fplot_angle(ion)
+      fplot_angle(ion, '')
 
       # redefine bins
       a_n_bins = 18
@@ -252,7 +207,7 @@ if __name__ == '__main__':
       r_bins = np.flip(r_bins, 0)
 
       # Step through each ion and make plots of radius vs N for 3 radial bins
-      radial_data = make_radial_profile(a_arr, r_arr, cdens_arr, a_bins, r_bins, a_n_bins, r_n_bins)
+      radial_data = make_radial_profile(r_arr, r_bins, r_n_bins, cdens_arr, r_arr, r_bins, r_n_bins, False)
       ion = finish_plot(field, COS_data, fn_head)
       # for i in range(9):
       #   color = 3*i
@@ -264,20 +219,20 @@ if __name__ == '__main__':
                      (10*0, 10*0 + 5), colors[0])
       plot_profile(r_bins, radial_data[17], '%s < Φ < %s degrees' % \
                      (5*17, 5*17 + 5), colors[3])
-      fplot_radius(ion)
+      fplot_radius(ion, '')
 
       # Make plot similar to paper of phi vs N
       r_n_bins = 3
       r_bins = np.linspace(80, 20, r_n_bins, endpoint=False)
       r_bins = np.flip(r_bins, 0)
 
-      cden_data, angle_data = angle_profile_big(a_arr, r_arr, cdens_arr, a_bins, r_bins, a_n_bins, r_n_bins)
+      cden_data, angle_data = angle_profile_big(a_arr, a_bins, a_n_bins, cdens_arr, r_arr, r_bins, r_n_bins)
       ion = finish_plot(field, COS_data, fn_head)
       plot_big_angle(angle_data[0], cden_data[0], 'b < 40 kpc', colors[0], markers[0])
       for i in range(1,3):
         plot_big_angle(angle_data[i], cden_data[i], '%s < b < %s kpc' % \
                     (i*20 + 20, i*20 + 40), colors[3*i], markers[i])
-      fplot_big_angle(ion)
+      fplot_big_angle(ion, 'big')
 
       # Makes plot similar to paper of b vs N
       r_n_bins = 4
@@ -287,10 +242,10 @@ if __name__ == '__main__':
       a_bins = np.linspace(90, 0, a_n_bins, endpoint=False)
       a_bins = np.flip(a_bins, 0)
 
-      cden_data, radius_data = radial_profile_big(a_arr, r_arr, cdens_arr, a_bins, r_bins, a_n_bins, r_n_bins)
+      cden_data, radius_data = big_profile(r_arr, r_bins, r_n_bins, cden_arr, a_arr, a_bins, a_n_bins)
       ion = finish_plot(field, COS_data, fn_head)
       plot_big_radius(radius_data[0], cden_data[0], 'Φ < 45 degrees', colors[0], markers[0])
       plot_big_radius(radius_data[1], cden_data[1], 'Φ > 45 degrees', colors[3], markers[1])
-      fplot_big_radius(ion)
+      fplot_big_radius(ion, 'big')
 
 
