025fd5fa6e6c36ea34bdbf9813336fca0fd80960
Andy Rothstein <arothste@caltech.edu>
2018-08-09 10:57:20 -0700

    Minor change

--- azimuthal_profile.py
+++ azimuthal_profile.py
@@ -72,7 +72,7 @@ def make_radial_profile(a_arr, r_arr, cdens_arr, a_bins, r_bins, a_n_bins, r_n_b
   a_bin_ids = np.digitize(a_arr, a_bins)
   angle_bins = range(a_n_bins)
   profile_data = np.array([])
-  for j in range(a_bins):
+  for j in range(a_n_bins):
     profile_data.append(np.zeros([3, r_n_bins]))
   for i, r_bin_id in enumerate(np.arange(len(r_bins))):
     for j in range(a_n_bins):
