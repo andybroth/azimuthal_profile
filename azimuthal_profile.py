b2ae1365e75fbdf9321cb3f7ec84720e73ee93d2
Andy Rothstein <arothste@caltech.edu>
2018-08-15 14:23:34 -0700

    Indent error

--- azimuthal_profile.py
+++ azimuthal_profile.py
@@ -225,7 +225,7 @@ if __name__ == '__main__':
         while color >= len(colors):
           color -= len(colors)
         plot_profile(r_bins, radial_data[i], '%s < Φ < %s degrees' % (angle*i, angle*i + angle), colors[color])
-        fplot_radius(ion, '')
+      fplot_radius(ion, '')
       
 
       # Test plot, should show much higher density in 75-90 degree bins over
