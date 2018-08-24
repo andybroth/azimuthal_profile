e56151648be971c536dfee77699af19440843ace
Andy Rothstein <arothste@caltech.edu>
2018-08-24 13:53:40 -0700

    New

--- ang_mom.py
+++ ang_mom.py
@@ -36,6 +36,12 @@ if __name__ == '__main__':
 	amiga_data = smooth_amiga(amiga_data)
 
 	for fn in yt.parallel_objects(fns):
+		fn = fn.strip() # Get rid of trailing \n
+		fn_head = fn.split('/')[-1]
+		fn_data = fn.split('/')[-4]
+		cdens_fn_1 = "%s/%s_1_cdens.h5" % (fn_data, fn_head)
+		cdens_fn_2 = "%s/%s_2_cdens.h5" % (fn_data, fn_head)
+
 		# Define ions we care about
 		ions = []
 		ion_fields = []
@@ -85,7 +91,6 @@ if __name__ == '__main__':
 		full_other_fields.append(('gas', 'temperature'))
 
 		log("Starting projections for %s" % fn)
-		import pdb; pdb.set_trace()
 		ds = GizmoDataset(fn)
 		trident.add_ion_fields(ds, ions=ions, ftype='gas')
 
@@ -101,13 +106,6 @@ if __name__ == '__main__':
 		ions.append('metal_density')
 		ion_fields.append('metal_density')
 		full_ion_fields.append(('gas', 'metal_density'))
-
-
-		fn = fn.strip() # Get rid of trailing \n
-		fn_head = fn.split('/')[-1]
-		fn_data = fn.split('/')[-4]
-		cdens_fn_1 = "%s/%s_1_cdens.h5" % (fn_data, fn_head)
-		cdens_fn_2 = "%s/%s_2_cdens.h5" % (fn_data, fn_head)
 
 		# Figure out centroid and r_vir info
 		log("Reading amiga center for halo in %s" % fn)
