db583248ab795cde159fae82b752b18e39fe6b04
Andy Rothstein <arothste@caltech.edu>
2018-08-31 13:48:40 -0700

    Print fn

--- ang_mom.py
+++ ang_mom.py
@@ -53,6 +53,7 @@ if __name__ == '__main__':
 		ions.append('O VI')
 
 		log("Starting projections for %s" % fn)
+		log(fn)
 		ds = GizmoDataset(fn)
 		trident.add_ion_fields(ds, ions=ions, ftype='gas')
 
