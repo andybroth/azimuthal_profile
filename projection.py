220a8768b534f66e41567ed0d1705800eae24f69
Andy Rothstein <arothste@caltech.edu>
2018-09-17 16:14:38 -0700

    Changes projections

--- projection.py
+++ projection.py
@@ -35,10 +35,12 @@ if __name__ == '__main__':
 
 	radial_extent = ds.quan(250, 'kpc')
 	width = 2*radial_extent
+	width1= width
 
 	log("Reading amiga center for halo in %s" % fn)
 	c = read_amiga_center(amiga_data, fn, ds)
 	rvir = read_amiga_rvir(amiga_data, fn, ds)
+	rvir1 = rvir
 
 	log('Finding Angular Momentum of Galaxy')
 	sp = ds.sphere(c, (15, 'kpc'))
@@ -51,14 +53,42 @@ if __name__ == '__main__':
 	log('Generating Plot 1')
 	p1 = yt.OffAxisProjectionPlot(ds, E1, ('gas', 'density'), center=c, 
 		width=width, data_source=box, north_vector=L, weight_field=None)
-	log('Generating Plot 2')
-	p2 = yt.OffAxisProjectionPlot(ds, E1, ('gas', 'H_number_density'), center=c, 
-		width=width, data_source=box, north_vector=L, weight_field=None)
 	log('Generating Plot 3')
 	p3 = yt.OffAxisProjectionPlot(ds, E1, ('gas', 'Mg_p1_number_density'), center=c, 
 		width=width, data_source=box, north_vector=L, weight_field=None)
+	
+	amiga_data = get_amiga_data('/mnt/data1/GalaxiesOnFire/metaldiff/m12f_res7100_md/halo/ahf/halo_00000_smooth.dat')
+	amiga_data = smooth_amiga(amiga_data)
+
+	fn = '/mnt/data1/GalaxiesOnFire/metaldiff/m12f_res7100_md/output/snapdir_600/snapshot_600.0.hdf5'
+	fn_head = fn.split('/')[-1]
+	fn_data = fn.split('/')[-4]
+
+	ions = ['Mg II']
+
+	log("Starting projections for %s" % fn)
+	ds = GizmoDataset(fn)
+	trident.add_ion_fields(ds, ions=ions, ftype='gas')
+
+	radial_extent = ds.quan(250, 'kpc')
+	width = 2*radial_extent
+	width2= width
+
+	log("Reading amiga center for halo in %s" % fn)
+	c = read_amiga_center(amiga_data, fn, ds)
+	rvir = read_amiga_rvir(amiga_data, fn, ds)
+	rvir2 = rvir
+
+	log('Finding Angular Momentum of Galaxy')
+	sp = ds.sphere(c, (15, 'kpc'))
+	L = sp.quantities.angular_momentum_vector(use_gas=False, use_particles=True, particle_type='PartType0')
+	L, E1, E2 = ortho_find(L)
+
+	log('Generating Plot 2')
+	p2 = yt.OffAxisProjectionPlot(ds, E1, ('gas', 'density'), center=c, 
+		width=width, data_source=box, north_vector=L, weight_field=None)
 	log('Generating Plot 4')
-	p4 = yt.OffAxisProjectionPlot(ds, E1, ('gas', 'O_p5_number_density'), center=c, 
+	p4 = yt.OffAxisProjectionPlot(ds, E1, ('gas', 'Mg_p1_number_density'), center=c, 
 		width=width, data_source=box, north_vector=L, weight_field=None)
 
 	print("Stitching together")
@@ -89,21 +119,22 @@ if __name__ == '__main__':
 		ax.yaxis.set_visible(False)
 
 		# Add virial radius circles to each plot
-		cir = Circle((0.5, 0.5), rvir/width, fill=False, color='white', linestyle='dashed', linewidth=5, transform=ax.transAxes, alpha=0.5)
+		if i % 2 == 0:
+			cir = Circle((0.5, 0.5), rvir1/width1, fill=False, color='white', linestyle='dashed', linewidth=5, transform=ax.transAxes, alpha=0.5)
+		else:
+			cir = Circle((0.5, 0.5), rvir2/width2, fill=False, color='white', linestyle='dashed', linewidth=5, transform=ax.transAxes, alpha=0.5)
 		ax.add_patch(cir)
 
 	row1_cax = fig.add_cax(fig[1], 'right', buffer=0.01, length=1, width=0.05)
 	row1_cbar = plt.colorbar(plot2, cax=row1_cax, orientation='vertical')
-	row1_cbar.set_label('H I Column Density [cm$^{-2}$]', weight='bold')
+	row1_cbar.set_label('Projected Density ($\frac{g}{cm^2}$)', weight='bold')
 	row1_cbar.ax.yaxis.label.set_font_properties(matplotlib.font_manager.FontProperties(size=16))
 	row1_cbar.ax.tick_params(labelsize=14)
 
 	row2_cax = fig.add_cax(fig[3], 'right', buffer=0.01, length=1, width=0.05)
 	row2_cbar = plt.colorbar(plot4, cax=row2_cax, orientation='vertical')
-	row2_cbar.set_label('Spatial Resolution [kpc]', weight='bold')
+	row2_cbar.set_label('Mg II Column Density (cm$^{-2}$)', weight='bold')
 	row2_cbar.ax.yaxis.label.set_font_properties(matplotlib.font_manager.FontProperties(size=16))
-	row2_cbar.set_ticks([6,7,8,9,10])
-	row2_cbar.set_ticklabels(['4', '2', '1', '0.5', '0.25'])
 	row2_cbar.ax.tick_params(labelsize=14) 
 
 	plt.savefig('projections.png')
